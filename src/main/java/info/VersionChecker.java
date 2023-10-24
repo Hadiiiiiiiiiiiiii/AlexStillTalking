42169a27d5325bf97a5097ac0ae9767971e8ca71
Hadiiiiiiiiiiiiii <k6m@hotmail.es>
2023-10-24 19:17:49 +0200

    update to game version 2.30.0.23, fixed version checker spamming the text channels

--- src/main/java/info/VersionChecker.java
+++ src/main/java/info/VersionChecker.java
@@ -13,6 +13,8 @@ import java.nio.file.Paths;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicReference;
+
 public class VersionChecker {
     private static final String URL_DEV = "https://yupmaster.gaijinent.com/yuitem/get_version.php?proj=warthunder&tag=dev";
     private static final String URL_LIVE = "https://yupmaster.gaijinent.com/yuitem/get_version.php?proj=warthunder";
@@ -22,8 +24,8 @@ public class VersionChecker {
     private static final Path VERSION_FILE_LIVE = Paths.get("Data/Other/gameVersion_live");
     private final JDA jda;
     private final OkHttpClient httpClient;
-    private String currentVersionDev;
-    private String currentVersionLive;
+    private final AtomicReference<String> currentVersionDev = new AtomicReference<>();
+    private final AtomicReference<String> currentVersionLive = new AtomicReference<>();
 
     public VersionChecker(JDA jda) {
         this.jda = jda;
@@ -33,10 +35,10 @@ public class VersionChecker {
     public void start() {
         try {
             if (Files.exists(VERSION_FILE_DEV)) {
-                currentVersionDev = Files.readString(VERSION_FILE_DEV).trim();
+                currentVersionDev.set(Files.readString(VERSION_FILE_DEV).trim());
             }
             if (Files.exists(VERSION_FILE_LIVE)) {
-                currentVersionLive = Files.readString(VERSION_FILE_LIVE).trim();
+                currentVersionLive.set(Files.readString(VERSION_FILE_LIVE).trim());
             }
         } catch (IOException e) {
             e.printStackTrace();
@@ -47,15 +49,16 @@ public class VersionChecker {
         executorService.scheduleAtFixedRate(() -> checkVersion(URL_LIVE, VERSION_FILE_LIVE, "live", currentVersionLive), 0, 10, TimeUnit.SECONDS);
     }
 
-    private void checkVersion(String url, Path versionFile, String versionType, String currentVersion) {
+
+    private void checkVersion(String url, Path versionFile, String versionType, AtomicReference<String> currentVersionRef) {
         try {
             Request request = new Request.Builder().url(url).build();
             Response response = httpClient.newCall(request).execute();
-
             if (response.isSuccessful()) {
                 String version = response.body().string().trim();
+                String currentVersion = currentVersionRef.get();
                 if (currentVersion == null) {
-                    currentVersion = version;
+                    currentVersionRef.set(version);
                 } else if (!currentVersion.equals(version)) {
                     String[] oldParts = currentVersion.split("\\.");
                     String[] newParts = version.split("\\.");
@@ -71,6 +74,7 @@ public class VersionChecker {
                             channel2.sendMessage("Major Game " + versionType + " version changed: " + version).queue();
                         }
                     }
+                    currentVersionRef.set(version);
 
                     currentVersion = version;
 
